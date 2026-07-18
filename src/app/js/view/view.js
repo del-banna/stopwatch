6cfb627806947d8f2eb617fa0b1b449d20e70e05
Del <aelbanna3896@gmail.com>
2026-07-17 20:51:03 -0400

    Move settings UI to new bar and refactor parent element assignment in view.js initialization

--- src/app/js/view/view.js
+++ src/app/js/view/view.js
@@ -378,26 +378,31 @@ export class StopwatchElement {
 
 export const wrapper = createElement("div", { id: "wrapper", classList: ['container'], parent: document.body });
 
-parent = wrapper;
-export const controlsDiv = createElement("div", { id: "controls", classList: ['flex'], parent });
-export const listElement = createElement("ul", { id: "list", classList: ['container'], parent });
+export const controlBar = createElement("div", { id: "controls", classList: ['flex', 'v-margin-5'], parent: wrapper });
 
-parent = createElement("div", { parent: controlsDiv, id: "add", classList: ['left', 'vc-container'] });
-export const createNewStopwatchButton = createMaterialIcon(materialDesign.icons.add, { id: "createNew", classList: ia_common1, parent });
-export const pasteButton = createMaterialIcon(materialDesign.icons.paste, { id: "paste", classList: ia_common1, parent });
-export const uploadButton = createMaterialIcon(materialDesign.icons.upload, { id: 'import', classList: ia_common1, parent });
+const inboundControlMenu = createElement("div", { parent: controlBar, id: "add", classList: ['left', 'vc-container'] });
+export const createNewStopwatchButton = createMaterialIcon(materialDesign.icons.add, { id: "createNew", classList: ia_common1, parent: inboundControlMenu });
+export const pasteButton = createMaterialIcon(materialDesign.icons.paste, { id: "paste", classList: ia_common1, parent: inboundControlMenu });
+export const uploadButton = createMaterialIcon(materialDesign.icons.upload, { id: 'import', classList: ia_common1, parent: inboundControlMenu });
 
-parent = createElement("div", { parent: controlsDiv, id: "collectiveControl", classList: ['middle', 'vc-container'] });
-export const resetAllButton = createMaterialIcon(materialDesign.icons.reset_all, { id: 'resetAll', classList: [...ia_common2, yellow], parent });
-export const resumeAllButton = createMaterialIcon(materialDesign.icons.resume_all, { id: 'resumeAll', classList: [...ia_common2, green], parent });
-export const pauseAllButton = createMaterialIcon(materialDesign.icons.pause_all, { id: 'pauseAll', classList: [...ia_common2, blue], parent });
-export const deleteAllButton = createMaterialIcon(materialDesign.icons.delete_all, { id: 'deleteAll', classList: [...ia_common2, red], parent });
+const collectionControlMenu = createElement("div", { parent: controlBar, id: "collectiveControl", classList: ['middle', 'vc-container'] });
+export const resetAllButton = createMaterialIcon(materialDesign.icons.reset_all, { id: 'resetAll', classList: [...ia_common2, yellow], parent: collectionControlMenu });
+export const resumeAllButton = createMaterialIcon(materialDesign.icons.resume_all, { id: 'resumeAll', classList: [...ia_common2, green], parent: collectionControlMenu });
+export const pauseAllButton = createMaterialIcon(materialDesign.icons.pause_all, { id: 'pauseAll', classList: [...ia_common2, blue], parent: collectionControlMenu });
+export const deleteAllButton = createMaterialIcon(materialDesign.icons.delete_all, { id: 'deleteAll', classList: [...ia_common2, red], parent: collectionControlMenu });
 
-parent = createElement("div", { parent: controlsDiv, id: "export", classList: ['right', 'vc-container'] });
-export const concurrencySwitch = createSwitchElement({ id: "concurrencySwitch", label: "Concurrency", classList: ia_common1, parent });
+const outboundControlMenu = createElement("div", { parent: controlBar, id: "export", classList: ['right', 'vc-container'] });
+export const linkButton = createMaterialIcon(materialDesign.icons.link, { id: "link", classList: ia_common1, parent: outboundControlMenu });
+export const copyAllButton = createMaterialIcon(materialDesign.icons.copy_all, { id: "copyAll", classList: ia_common1, parent: outboundControlMenu });
+export const copyLinkButton = createMaterialIcon(materialDesign.icons.copy_link, { id: "copyLink", classList: ia_common1, parent: outboundControlMenu });
+export const downloadAllButton = createMaterialIcon(materialDesign.icons.download, { id: "export", classList: ia_common1, parent: outboundControlMenu });
+
+
+const settingsBar = createElement("div", { id: "settings", classList: ['flex', 'v-margin-5'], parent: wrapper });
+const settingsMenu = createElement("div", { id: "settings", classList: ['middle', 'vc-container'], parent: settingsBar });
+export const concurrencySwitch = createSwitchElement({ id: "concurrencySwitch", label: "Concurrency", classList: ia_common1, parent: settingsMenu });
 concurrencySwitch.labelElement.hidden = true; // Feature not yet implemented
-export const dynamicURLSwitch = createSwitchElement({ id: "dynamicLinkSwitch", label: "Dynamic URL", classList: ia_common1, parent });
-export const linkButton = createMaterialIcon(materialDesign.icons.link, { id: "link", classList: ia_common1, parent });
-export const copyAllButton = createMaterialIcon(materialDesign.icons.copy_all, { id: "copyAll", classList: ia_common1, parent });
-export const copyLinkButton = createMaterialIcon(materialDesign.icons.copy_link, { id: "copyLink", classList: ia_common1, parent });
-export const downloadAllButton = createMaterialIcon(materialDesign.icons.download, { id: "export", classList: ia_common1, parent });
+export const dynamicURLSwitch = createSwitchElement({ id: "dynamicLinkSwitch", label: "Dynamic URL", classList: ia_common1, parent: settingsMenu });
+
+
+export const listElement = createElement("ul", { id: "list", classList: ['container'], parent: wrapper });
